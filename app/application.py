05eb4cc4860db4b7875d5c3ef51c3b2643a5add5
Alixe54 <ali.jafari.2114@gmail.com>
2025-05-27 01:19:54 +0330

    updating project

--- app/application.py
+++ app/application.py
@@ -1,3 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('base.html')
