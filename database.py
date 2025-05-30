7f56b9ef08052a3f615b6300e40d13a5f79b48ea
Arash Manouchehri <arashmanouchehri83@gmail.com>
2025-05-30 11:19:13 +0330

    add database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+from json import load, dump
+from pprint import pprint
+class Database:
+    
+    def __init__(self):
+        with open('db.json', 'r') as f:
+            self.data = load(f)
+
+    def save(self):
+        with open('db.json', 'w') as f:
+            dump(self.data)
+
+db = Database()
+books = db.data.get('books')
