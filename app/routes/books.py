cf7332b15992151ca6573c913f2083e8aa7bb330
JaVaD15Mesgarha <Mohammadjavadmesgarha@gmail.com>
2025-05-31 18:33:18 +0330

    finalize project

--- app/routes/books.py
+++ app/routes/books.py
@@ -1,36 +1,55 @@
 from app.application import app
-from flask import jsonify
+from flask import jsonify,request
+from .database  import *
+from werkzeug.exceptions import NotFound , BadRequest
 
 @app.route("/api/v1/books")
 def get_books():
-    """
-    Get all books
-
-    Returns:
-        list: List of books
-    """
-    return jsonify({"books": []})
+    database = load_data()
+    books = database["books"] 
+    return jsonify({"books": books})
 
 
 @app.route("/api/v1/books/<book_id>")
 def get_book(book_id: str):
-    """
-    Get a book by id
+    database = load_data()
+    books = database["books"]
 
-    Args:
-        book_id (str): The id of the book
+    for book in books :
+      if book["id"] == book_id :
+        return jsonify({"book": book})
 
-    Returns:
-        dict: Book details
-
-    Raises:
-        NotFound: If the book is not found
-    """
-    return jsonify({"book": {}})
-
+    raise NotFound("book not found")
 
 @app.route("/api/v1/books", methods=["POST"])
 def create_book():
+    data = request.get_json()
+
+    if not data :
+       raise BadRequest("No json body")
+    
+    required_fields = ["title" , "author" , "ISBN"]
+    for fields in required_fields :
+       if fields not in data :
+          raise BadRequest("Missing fields")
+    database = load_data()
+    books = database["books"]
+    
+    id = 0
+    for _ in books :
+       id += 1
+       
+    new_book = {
+        "id" : f"{id}" ,
+        "title" : data["title"] , 
+        "author" : data["author"] , 
+        "isbn" : data["isbn"],
+        "is_reserved": False ,
+        "reserved_by": None , 
+    }
+
+    return jsonify({"book": new_book})
+
     """
     Create a new book
 
@@ -40,11 +59,20 @@ def create_book():
     Raises:
         BadRequest: If the request body is invalid
     """
-    return jsonify({"book": {}})
-
 
 @app.route("/api/v1/books/<book_id>", methods=["DELETE"])
 def delete_book(book_id: str):
+    database = load_data() 
+    books = database["books"]
+    for book in books :
+       if book_id == book["id"] :
+          books.remove(book)
+    
+    for book in books :
+       if book["id"] < book_id :
+           book["id"] -= 1
+
+    return jsonify({"message": "Book deleted"})
     """
     Delete a book by id
 
@@ -58,4 +86,4 @@ def delete_book(book_id: str):
         NotFound: If the book is not found
         BadRequest: If the book is reserved
     """
-    return jsonify({"message": "Book deleted"})
+
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,37 +1,53 @@
-from flask import jsonify
+from flask import jsonify ,request
 from app.application import app
-
+from .database import *
+from werkzeug.exceptions import NotFound , BadRequest
 
 @app.route("/api/v1/users")
 def get_users():
-    """
-    Get all users
-
-    Returns:
-        list: List of users
-    """
-    return jsonify({"users": []})
+    database = load_data()
+    users = database["users"] 
+    return jsonify({"users": users})
 
 
 @app.route("/api/v1/users/<user_id>")
 def get_user(user_id: str):
-    """
-    Get a user by id
-
-    Args:
-        user_id (str): The id of the user
-
-    Returns:
-        dict: User details
-
-    Raises:
-        NotFound: If the user is not found
-    """
-    return jsonify({"user": {}})
+    database = load_data()
+    users = database["books"]
+    for user in users :
+      if user["id"] == user_id :
+        return jsonify({"user": user})
+    raise NotFound("book not found")
 
 
 @app.route("/api/v1/users", methods=["POST"])
 def create_user():
+    data = request.get_json()
+
+    if not data :
+       raise BadRequest("No json body")
+    
+    required_fields = ["username" , "name" , "email"]
+    for fields in required_fields :
+       if fields not in data :
+          raise BadRequest("Missing fields")
+    
+    database = load_data()
+    users = database["users"]
+    
+    id = 0
+    for _ in users :
+       id += 1
+       
+    new_user = {
+        "id" : f"{id}" ,
+        "username" : data["username"] , 
+        "name" : data["name"] , 
+        "email" : data["email"],
+        "reserved_books" : [] , 
+    }
+
+    return jsonify({"user": new_user})
     """
     Create a new user
 
@@ -41,11 +57,24 @@ def create_user():
     Raises:
         BadRequest: If the request body is invalid
     """
-    return jsonify({"user": {}})
 
 
 @app.route("/api/v1/users/<user_id>", methods=["PUT"])
 def update_user(user_id: str):
+    database = load_data()
+    users = database["users"]
+    data = request.get_json()
+
+    if not data :
+       raise BadRequest
+    
+    for user in users :
+        if user_id == user["id"] :
+            for key , value in data.items() :
+                if key in user :
+                    user[key] = value
+            return jsonify({"user": user})
+    raise NotFound("not found")
     """
     Update a user by id
 
@@ -59,4 +88,3 @@ def update_user(user_id: str):
         BadRequest: If the request body is invalid
         NotFound: If the user is not found
     """
-    return jsonify({"user": {}})
