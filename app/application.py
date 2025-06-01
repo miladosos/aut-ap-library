bf76f9f5da99ae868e3db348cbb35d0ed3c3a74c
amin9910 <www.aminhashemicr7@gmail.com>
2025-06-01 13:09:55 +0330

    Update application.py

--- app/application.py
+++ app/application.py
@@ -1,16 +1,24 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__, static_folder="static", static_url_path="")
 CORS(app)
 
+# Error handler to return JSON for HTTPExceptions
+@app.errorhandler(HTTPException)
+def handle_http_exception(e):
+    response = jsonify(message=e.description)
+    response.status_code = e.code
+    return response
+
+# Import routes to register endpoints
+from app.routes import books, users, reservation
 
 @app.route("/")
 def index():
     return send_from_directory("static", "index.html")
 
-
-# Add a route to serve static files
 @app.route("/<path:path>")
 def serve_static(path):
     return send_from_directory("static", path)
